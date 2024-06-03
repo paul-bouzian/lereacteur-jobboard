f136f7fd17d2e0ad37f879c61d2e6b08161c3e2d
Paul Bouzian <paulbouzian@mbp-de-paul.home>
2024-06-03 13:25:09 +0200

    finished exercice

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
 const Footer = () => {
   return (
     <footer>
-      <span>Made with</span> <span>React</span> <span>by</span>{" "}
-      <span>Nono</span>
+      <span>Made with</span> <span className="bold">React</span> <span>at</span>{" "}
+      <span className="bold">Le Reacteur</span> <span>by</span>
+      <span className="bold">Paul Bouzian</span>
     </footer>
   );
 };
